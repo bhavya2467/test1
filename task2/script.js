7d95ee4e5ad1bde277363160bdafb7e84b75e1cf
bhavya2467 <kilaribhavya246@gmail.com>
2025-04-29 18:33:56 +0530

    Add files via upload

---
+++ task2/script.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-toggle").addEventListener("click", function() {
+    let sidebar = document.getElementById("sidebar");
+    sidebar.style.display = sidebar.style.display === "block" ? "none" : "block";
+});
